b15b7f37acc4e0d2ee6905ae77ffc40d0026df99
Maicon Souza <maicon_souza09@hotmail.com>
2022-09-26 12:42:43 -0300

    operadores logicos

---
+++ aula21/operadoresDeComparacao.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+== igualdade
+=== igualdade estrita
+!= diferente
+!== diferente estrito
+ */
+
+console.log(10 > 8)
+console.log('a' > 'A')
+console.log('5' == 5)
+console.log('5' === 5)
+console.log( undefined == null)
+console.log( undefined === null)
+
---
+++ aula22/operadoresLogicos.js
@@ -0,0 +1,11 @@
+/* Operadores logicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+console.log(true && true && true)
+console.log(true && false && true)
+
+console.log(false || false || false)
+console.log(false || false || true)
